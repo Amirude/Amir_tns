9d43b89a573ff6dac53aef141ee1ba3d0ba03362
amiru <amiru@172.16.224.240>
2025-08-06 15:10:59 +0530

    first commit

---
+++ TNDS/src/fs/core/FirstProgram.java
@@ -0,0 +1,8 @@
+package fs.core;
+import java.util.LinkedList ; //all subpackages
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
